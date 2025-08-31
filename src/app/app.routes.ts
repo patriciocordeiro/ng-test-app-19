3ea54ee2f9135d485d142327f346a94ec51987a1
patriciocordeiro <paponcio6@gmail.com>
2025-08-31 15:44:42 +0100

    chore: fixing routing error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppRoutes } from './core/constanst/routes.constants';
-import { TASK_ROUTES } from './features/task/task.routes';
 
 export const routes: Routes = [
-  {
-    path: AppRoutes.TASKS,
-    loadComponent: () =>
-      import('./features/task/task-layout/task-layout.component').then(c => c.TaskLayoutComponent),
-    children: TASK_ROUTES,
-  },
   {
     path: AppRoutes.ROOT,
     redirectTo: AppRoutes.TASKS,
     pathMatch: 'full',
+  },
+  {
+    path: AppRoutes.TASKS,
+    loadChildren: () => import('./features/task/task.routes').then(m => m.TASK_ROUTES),
   },
 ];
--- src/app/features/task/task.routes.ts
+++ src/app/features/task/task.routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
+import { TaskListPageComponent } from './pages/task-list-page/task-list-page.component';
 
 export const TASK_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./pages/task-list-page/task-list-page.component').then(c => c.TaskListPageComponent),
-  },
-  {
-    path: ':id',
-    loadComponent: () =>
-      import('./pages/task-detail-page/task-detail-page.component').then(
-        c => c.TaskDetailPageComponent,
-      ),
+    component: TaskListPageComponent,
+
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/task-list-page/task-list-page.component').then(
+            c => c.TaskListPageComponent,
+          ),
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./pages/task-detail-page/task-detail-page.component').then(
+            c => c.TaskDetailPageComponent,
+          ),
+      },
+    ],
   },
 ];
